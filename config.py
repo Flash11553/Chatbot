4b2f8780773c2dd7903a75287521f12d3d80d585
xSanalOwner <legendmuzkao99@gmail.com>
2024-08-07 23:54:28 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", 24049614))
 API_HASH = getenv("API_HASH", "e1936f93366810f1bc6eb38fa6e6f49c")
-BOT_TOKEN = getenv("BOT_TOKEN", "67393095762:AAEXpr7nkCgM0nylUatWudAm7QN36aS0-fg")
+BOT_TOKEN = getenv("BOT_TOKEN", "7393095762:AAHW7dKK9eve7al2kStlru2vEabhWOU4rYY")
 OWNER_ID = int(getenv("OWNER_ID", "7406471382"))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://NigarChatBot:NigarChatBot@nigarchatbot.pbfos1e.mongodb.net/?retryWrites=true&w=majority")
 SUPPORT_GRP = getenv("SUPPORT_GRP", "sohbetazerbaycanteamchat")
