037dc3c5f842e0b1de6ed65cf56eb595505170cb
xSanalOwner <legendmuzkao99@gmail.com>
2025-03-01 13:47:37 +0400

    Update inline.py

--- Venom/modules/helpers/inline.py
+++ Venom/modules/helpers/inline.py
@@ -22,7 +22,7 @@ DEV_OP = [
    
     [
        InlineKeyboardButton(text="Kömək 🔮", callback_data="HELP"),
-       InlineKeyboardButton(text="Support 💬", url=f"https://t.me/{SUPPORT_GRP}"),
+       InlineKeyboardButton(text="Support 💬", url=f"https://t.me/{SUPPORT_CHNL}"),
     ],
 ]
 
