5a6d00649be5204e68c1e2dc6f24b694cc528c04
xSanalOwner <legendmuzkao99@gmail.com>
2025-03-01 12:34:46 +0400

    Update read.py

--- Venom/modules/helpers/read.py
+++ Venom/modules/helpers/read.py
@@ -36,11 +36,3 @@ CHATBOT_READ = f"""
 SOURCE_READ = f"**Salam, [{VenomX.name}](https://t.me/{VenomX.username})- in mənbə kodu aşağıda verilmişdir.**\n**XAHIŞ EDİRƏM REPO-Nİ FORK EDİN VƏ YILDIZI VERİN ✯**\n**──────────────────**\n**MƏNBƏ KODU [burada](https://github.com/venombolteop/ChatbotV2)**\n**──────────────────**\n**ƏGƏR HANSI-SA PROBLEMİ BAŞ VERƏRSƏ, ONDAN SONRA [DƏSTƏK QURULUŞUNA](https://t.me/{SUPPORT_GRP}) MÜRACİƏT EDİN.\n<b>||©️ @{OWNER_USERNAME}||</b>"
 
 ADMIN_READ = f"Yenidir"
-
-ABOUT_READ = f"""
-**➻ [{VenomX.name}](https://t.me/{VenomX.username})- in bir çat-botdur.**
-**➻ [{VenomX.name}](https://t.me/{VenomX.username})- avtomatik olaraq istifadəçiyə cavab verir.**
-**➻ Qruplarınızı aktivləşdirməyinizə kömək edir.**
-**➻ [Python](https://www.python.org) ilə yazılıb [MongoDB](https://www.mongodb.com) kimi verilənlər bazası ilə.**
-**➻ [{VenomX.name}](https://t.me/{VenomX.username})- haqqında əsas kömək və məlumat üçün aşağıda verilmiş düymələrə basın**
-"""
