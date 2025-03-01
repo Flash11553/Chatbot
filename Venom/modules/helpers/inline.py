5b60d525347303555600e283e05741e14ed9d6d9
xSanalOwner <legendmuzkao99@gmail.com>
2025-03-01 12:31:32 +0400

    Update inline.py

--- Venom/modules/helpers/inline.py
+++ Venom/modules/helpers/inline.py
@@ -103,13 +103,3 @@ HELP_START = [
         InlineKeyboardButton(text="çɪxɪş 🚫", callback_data="CLOSE"),
     ],
 ]
-
-
-HELP_BUTN = [
-    [
-        InlineKeyboardButton(
-            text="ᴋöᴍəᴋ 🔮", url=f"https://t.me/{VenomX.username}?start=help"
-        ),
-        InlineKeyboardButton(text="çɪxɪş 🚫", callback_data="CLOSE"),
-    ],
-]
